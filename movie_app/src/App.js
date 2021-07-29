f732270276abe27ea6a623c3f208050d611113ca
junsuboy <cobijraynor@naver.com>
2021-07-29 16:18:08 +0900

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,16 +1,50 @@
 import React from "react";
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
-      <Food fav="kimchi" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
+
+const foodILike = [
+  {
+    name: "kimchi",
+    image:
+      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+  },
+  {
+    name: "ramen",
+    image:
+      "https://i.huffpost.com/gen/1410937/thumbs/o-RAMEN-facebook.jpg#Ramen%202000x1000",
+  },
+  {
+    name: "samgyeopsal",
+    image:
+      "https://www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg",
+  },
+  {
+    name: "kimbap",
+    image:
+      "https://www.maangchi.com/wp-content/uploads/2007/08/gimbap_plate.jpg",
+  },
+  {
+    name: "bibimbap",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/1200px-Dolsot-bibimbap.jpg",
+  },
+];
 
 export default App;
