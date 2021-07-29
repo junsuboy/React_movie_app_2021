d39dbd87a6a534fc2252067b382d312eb391328f
junsuboy <cobijraynor@naver.com>
2021-07-29 19:00:45 +0900

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,62 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image:
-      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-    rating: 3.3
-  },
-  {
-    id: 2,
-    name: "ramen",
-    image:
-      "https://i.huffpost.com/gen/1410937/thumbs/o-RAMEN-facebook.jpg#Ramen%202000x1000",
-    rating: 4.4
-  },
-  {
-    id: 3,
-    name: "samgyeopsal",
-    image:
-      "https://www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg",
-    rating: 5.5
-  },
-  {
-    id: 4,
-    name: "kimbap",
-    image:
-      "https://www.maangchi.com/wp-content/uploads/2007/08/gimbap_plate.jpg",
-    rating: 5.0
-  },
-  {
-    id: 5,
-    name: "bibimbap",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/1200px-Dolsot-bibimbap.jpg",
-    rating: 4.1
-  },
-];
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
 
-
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
-      ))}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Plus</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
