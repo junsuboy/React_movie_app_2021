e0e0dfb9a8db1dd3b7d75b0297ae40bbd5780fdb
junsuboy <cobijraynor@naver.com>
2021-08-03 15:42:57 +0900

    Update Routing - go detail page

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 
 function App() {
@@ -9,9 +10,8 @@ function App() {
     <HashRouter>
       <Navigation />
       <Route path="/" exact={true} component={Home}></Route>
-      <Route path="/about">
-        <h1>About</h1>
-      </Route>
+      <Route path="/about" component={About}></Route>
+      <Route path="/movie/:id" component={Detail}></Route>
     </HashRouter>
   );
 }
