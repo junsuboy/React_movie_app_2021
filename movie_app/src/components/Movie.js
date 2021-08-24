19c722bf92daca206e841d7b25a7f03c45e05938
junsuboy <cobijraynor@naver.com>
2021-08-24 20:16:07 +0900

    css 오류수정/컴포넌트별 분할 저장

--- movie_app/src/components/Movie.js
+++ movie_app/src/components/Movie.js
@@ -5,34 +5,34 @@ import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <Link
-      to={{
-        pathname: `/movie/${id}`,
-        state: {
-          year,
-          title,
-          summary,
-          poster,
-          genres,
-        },
-      }}
-    >
-      <div className="movie">
-        <img src={poster} alt={title} title={title}></img>
+    <div className="movie">
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres,
+          },
+        }}
+      >
+        <img src={poster} alt={title} title={title} />
         <div className="movie__data">
           <h3 className="movie__title">{title}</h3>
           <h5 className="movie__year">{year}</h5>
           <ul className="movie__genres">
             {genres.map((genre, index) => (
-              <li key={index} className="genres">
+              <li key={index} className="genres__genre">
                 {genre}
               </li>
             ))}
           </ul>
-          <p className="movie__summary">{summary.slice(0, 140)}... </p>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
 
